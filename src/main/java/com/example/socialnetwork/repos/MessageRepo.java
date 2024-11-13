b4efc2306ef21f21533015b20c476302e4672815
Vladimir Bykov <Voviihb@gmail.com>
2024-11-13 11:11:32 +0300

    add filter for messages

--- src/main/java/com/example/socialnetwork/repos/MessageRepo.java
+++ src/main/java/com/example/socialnetwork/repos/MessageRepo.java
@@ -3,5 +3,8 @@ package com.example.socialnetwork.repos;
 import com.example.socialnetwork.domain.Message;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface MessageRepo extends CrudRepository<Message, Long> {
+    List<Message> findByTag(String tag);
 }
