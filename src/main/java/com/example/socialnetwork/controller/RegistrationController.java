14b998a1309724b48ab6e73f12f5c5fb9c2fa0aa
Vladimir Bykov <Voviihb@gmail.com>
2024-11-16 19:40:23 +0300

    1. migrate to .ftlh markdown 2. add filter as param to main

--- src/main/java/com/example/socialnetwork/controller/RegistrationController.java
+++ src/main/java/com/example/socialnetwork/controller/RegistrationController.java
@@ -15,16 +15,18 @@ import java.util.Map;
 public class RegistrationController {
     @Autowired
     private UserRepo userRepo;
+
     @GetMapping("/registration")
-    public String registration(){
+    public String registration() {
         return "registration";
     }
+
     @PostMapping("/registration")
-    public String addUser(User user, Map<String, Object> model){
+    public String addUser(User user, Map<String, Object> model) {
         User userFromDb = userRepo.findByUsername(user.getUsername());
-        if (userFromDb != null){
-           model.put("message", "User exists");
-           return "registration";
+        if (userFromDb != null) {
+            model.put("message", "Пользователь существует");
+            return "registration";
         }
         user.setActive(true);
         user.setRoles(Collections.singleton(Role.USER));
